cd99ac79aa7a325e3479db6cc22e91578d34c97c
Aryan-Labbalabadubdub <aryanhemmati1382@gmail.com>
2024-07-10 15:50:46 +0330

    Cleaned up code and improved extendability. Added Game backgrounds and FPS counter. Minor changes in collision handling

--- src/main/java/view/containers/PanelB.java
+++ src/main/java/view/containers/PanelB.java
@@ -2,50 +2,30 @@ package view.containers;
 
 import view.Utils;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-import static controller.constants.DefaultMethods.GET_AVERAGE_TONE_OF_CHARACTER;
 import static controller.constants.DimensionConstants.SCREEN_SIZE;
-import static controller.constants.FilePaths.MENU_IMAGEPATH;
+import static controller.constants.FilePaths.MENU_BACKGROUND_PATH;
+import static view.Utils.*;
 import static view.containers.GlassFrame.getGlassFrame;
 
 public class PanelB extends JPanel {
-    static final BufferedImage defaultImage;
-
-    static {
-        try {
-            BufferedImage image=ImageIO.read(new File(MENU_IMAGEPATH.getValue()));
-            float scale= (float) image.getWidth()/image.getHeight();
-            defaultImage = Utils.toBufferedImage(image.getScaledInstance((int) (SCREEN_SIZE.getValue().height*scale), SCREEN_SIZE.getValue().height, Image.SCALE_SMOOTH));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static final boolean BACKGROUND_RESIZE=false;
-    public BufferedImage imageSave;
-    public BufferedImage currentImage;
-    public GridBagConstraints constraints = new GridBagConstraints();
+    private static final boolean BACKGROUND_RESIZE=false;
+    private static final BufferedImage defaultImage=toBufferedImage(MENU_BACKGROUND_PATH.getValue());
+    transient BufferedImage imageSave;
+    transient BufferedImage currentImage;
+    private final GridBagConstraints constraints = new GridBagConstraints();
 
     public PanelB(Dimension dimension) {
-        this(dimension,background(dimension));
-    }
-    public static BufferedImage background(Dimension dimension){
-        if (BACKGROUND_RESIZE) return Utils.toBufferedImage(defaultImage.getScaledInstance(dimension.width, dimension.height, Image.SCALE_SMOOTH));
-        else return defaultImage.getSubimage((defaultImage.getWidth()-dimension.width)/2,(defaultImage.getHeight()-dimension.height)/2,dimension.width,dimension.height);
+        this(dimension, defaultImage);
     }
 
     public PanelB(Dimension dimension, BufferedImage image) {
-        this.currentImage = image;
-        this.imageSave = image;
+        setImage(image);
         setSize(dimension);
         setBackground(new Color(0, 0, 0, 0));
         setLayout(new GridBagLayout());
@@ -53,11 +33,11 @@ public class PanelB extends JPanel {
         getGlassFrame().add(this);
         setVisible(false);
 
-        constraints.insets = new Insets(10, 10, 10, 10);
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.anchor = GridBagConstraints.CENTER;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
+        getConstraints().insets = new Insets(10, 10, 10, 10);
+        getConstraints().fill = GridBagConstraints.BOTH;
+        getConstraints().anchor = GridBagConstraints.CENTER;
+        getConstraints().gridx = 0;
+        getConstraints().gridy = 0;
     }
 
     public PanelB(int width, int height, BufferedImage image) {
@@ -65,36 +45,35 @@ public class PanelB extends JPanel {
     }
 
     public void add(Component component, boolean nextHorizontal, boolean nextVertical) {
-        if (nextHorizontal) constraints.gridx++;
+        if (nextHorizontal) getConstraints().gridx++;
         if (nextVertical) {
-            constraints.gridy++;
-            constraints.gridx = 0;
+            getConstraints().gridy++;
+            getConstraints().gridx = 0;
         }
-        add(component, constraints);
+        add(component, getConstraints());
     }
 
-    public void bulkAdd(CopyOnWriteArrayList<Component> components, int itemsPerRow) {
+    public void bulkAdd(List<Component> components, int itemsPerRow) {
         while (!components.isEmpty()) {
             List<Component> rowItems = components.subList(0, Math.min(components.size(), itemsPerRow));
             float preferredAspectRatio = (float) rowItems.get(0).getPreferredSize().height / rowItems.get(0).getPreferredSize().width;
-            PanelB temp = new PanelB(new Dimension(getWidth(), (int) (preferredAspectRatio * getWidth()))) {{
-                setVisible(true);
-            }};
+            PanelB temp = new PanelB(new Dimension(getWidth(), (int) (preferredAspectRatio * getWidth())));
+            temp.setVisible(true);
             temp.horizontalBulkAdd(rowItems);
             add(temp, false, false);
             components.removeAll(rowItems);
-            constraints.gridx = 0;
-            constraints.gridy++;
+            getConstraints().gridx = 0;
+            getConstraints().gridy++;
         }
     }
 
     public void verticalBulkAdd(Collection<Component> components) {
-        constraints.gridy = -1;
+        getConstraints().gridy = -1;
         for (Component component : components) add(component, false, true);
     }
 
     public void horizontalBulkAdd(Collection<Component> components) {
-        constraints.gridx = -1;
+        getConstraints().gridx = -1;
         for (Component component : components) add(component, true, false);
     }
 
@@ -102,11 +81,25 @@ public class PanelB extends JPanel {
         setVisible(!isVisible());
     }
 
+    public void setImage(BufferedImage image){
+        this.imageSave = bufferedImageClone(image);
+        this.currentImage = bufferedImageClone(image);
+    }
+
     @Override
     public void setSize(int width, int height) {
-        super.setSize(width, height);
-        if (imageSave != null) currentImage = Utils.toBufferedImage(imageSave.getScaledInstance(width, height, Image.SCALE_SMOOTH));
-        setLocation((SCREEN_SIZE.getValue().width - width) / 2, (SCREEN_SIZE.getValue().height - height) / 2);
+        if (imageSave != null && BACKGROUND_RESIZE) currentImage = Utils.toBufferedImage(imageSave.getScaledInstance(width, height, Image.SCALE_SMOOTH));
+        setBounds((SCREEN_SIZE.getValue().width - width) / 2, (SCREEN_SIZE.getValue().height - height) / 2,width,height);
+    }
+
+    @Override
+    public void setBounds(int x, int y, int width, int height) {
+        super.setBounds(x, y, width, height);
+        if (imageSave!=null && !BACKGROUND_RESIZE) currentImage=cropImage(new Point(x,y),new Dimension(width,height),imageSave,BACKGROUND_RESIZE);
+    }
+
+    public GridBagConstraints getConstraints() {
+        return constraints;
     }
 
     @Override
@@ -114,5 +107,4 @@ public class PanelB extends JPanel {
         super.paintComponent(g);
         if (currentImage != null) g.drawImage(currentImage, 0, 0, getWidth(), getHeight(), null);
     }
-
 }
